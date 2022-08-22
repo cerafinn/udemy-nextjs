b4d9c27ecc0099b65e3b30418c074b87277ef5a7
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-08-22 16:21:35 -0400

    working on css module and linking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetups from './pages/AllMeetups';
+import Favourites from './pages/Favourites';
+import NewMeetup from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
+
+
 function App() {
-  return <div></div>;
+
+  return (
+  <div>
+    <MainNavigation />
+    <Switch>
+      <Route path='/' exact><AllMeetups /></Route>
+      <Route path='/new-meetup'><NewMeetup /></Route>
+      <Route path='/favourites'><Favourites /></Route>
+    </Switch>
+  </div>
+  );
 }
 
 export default App;
