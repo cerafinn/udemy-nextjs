f804e49771050504b0a1dd8358937056fa014c12
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-08-29 16:12:37 -0400

    added 404 page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,17 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  function loadProject() {
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'projecta'}
+    });
+  }
+
   return (
     <div>
       <h1>Projects of Given Client</h1>
+      <button onClick={loadProject}>Load Project A</button>
     </div>
   )
 }
