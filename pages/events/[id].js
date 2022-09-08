e39d3271664f3ddbedcccad31aab008621919ae2
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-09-08 17:26:28 -0400

    working on next.js project

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,4 +1,18 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../test-data';
+
 function EventDetails() {
+  const router = useRouter();
+
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <p>No event found!</p>
+    )
+  }
+
   return (
     <div>
       <h1>Event Details</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { getFeaturedEvents } from '../test-data';
+import EventList from '../components/events/event-list';
+
 function Homepage() {
+  const featuredEvents = getFeaturedEvents();
+
+
   return (
     <div>
-      <h1>Homepage</h1>
+      <EventList items={featuredEvents} />
     </div>
   )
 }
